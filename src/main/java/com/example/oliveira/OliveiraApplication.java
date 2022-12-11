cca61c85a3bdc65350365668fbba395bd2f3b5d2
Daniel Oliveira <apenasdanieloliveira@gmail.com>
2022-12-11 10:06:14 -0300

    base inicial do projeto com spring security

---
+++ src/main/java/com/example/oliveira/OliveiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.oliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveiraApplication.class, args);
+	}
+
+}
